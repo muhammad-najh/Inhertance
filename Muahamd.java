4ba2fae476203c820978fac9bd1bcd7ade803627
nuhammad-najh-faqo <40139929+nuhammad-najh-faqo@users.noreply.github.com>
2018-07-05 15:44:34 +0300

    Add files via upload

---
+++ Muahamd.java
@@ -0,0 +1,34 @@
+package com.example.lenovo.test_5_7inhertance;
+
+public class Muahamd extends Student{
+    @Override
+    public void setName(String name) {
+        this.name=name;
+        super.setName(name);
+    }
+
+    @Override
+    public int getStage() {
+        return super.getStage();
+    }
+
+    @Override
+    public void setStage(int stage) {
+        this.stage=stage;
+        super.setStage(stage);
+    }
+
+    @Override
+    public void displayInformation(String name) {
+        this.name=name;
+        super.displayInformation(name);
+        Object obj;
+    }
+
+    @Override
+    public void displayInformation(String name, int stage) {
+        this.name=name;
+        this.stage=stage;
+        super.displayInformation(name, stage);
+    }
+}
---
+++ Student.java
@@ -0,0 +1,40 @@
+package com.example.lenovo.test_5_7inhertance;
+
+import android.util.Log;
+
+public class Student {
+   String name;
+    int stage;
+
+    public Student(){
+
+    }
+
+    public Student(String name, int stage) {
+        this.name = name;
+        this.stage = stage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    public void displayInformation(String name){
+        Log.i("displayInformation","student Name : "+name);
+    }
+    public void displayInformation(String name,int stage){
+        Log.i("displayInformation","student Name : "+name+" Student age "+stage);
+    }
+}
